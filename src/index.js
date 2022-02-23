f94684ff5e927a76969811911d56505d296bc2ec
omar mohammed <omar.quhtuba95@gmail.com>
2022-02-23 10:21:16 -0800

    twelve

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL,{useUnifiedTopology: true,
+  useNewUrlParser: true
+})
 .then(() => console.log('mongoDB is connected successfully'))
 .catch((err) => console.log(err));
 
